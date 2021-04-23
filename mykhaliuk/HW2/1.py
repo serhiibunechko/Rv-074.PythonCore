0bab6616d2a1e0bfd8cdebf418f5cf165c34ed2a
Dmytro <dima_mihaliuk@ukr.net>
2021-04-23 12:14:49 +0300

    “homework1-2”

---
+++ mykhaliuk/HW2/1.py
@@ -0,0 +1,20 @@
+a = int(input(' a = ')) 
+b = int(input(' b = '))
+
+print('\n a + b = ', a + b,
+      '\n a - b = ', a - b,
+      '\n a * b = ', a * b,
+      '\n a / b = ', a / b,)
+
+name = input('\n What is your name? ')
+age  = int(input(' How old are you? '))
+city = input(' Where do you live? ')
+
+print('\n Hello,', name, '\n Your age is', age, '\n You live in', city)
+
+print ('        *  *     \n',
+       '     *      *    \n',
+       '    *  0  0  *   \n',
+       '   *    __/   *  \n',
+       '    *        *   \n',
+       '       *  *      \n')
---
+++ mykhaliuk/HW3/2.py
@@ -0,0 +1,5 @@
+bytes_value = float(input (" amount of bytes = "))
+
+kilobytes = bytes_value / 1024
+megabytes = kilobytes / 1024
+print(bytes_value, 'bytes:', '\n kilobytes', kilobytes, '\n megabytes', megabytes)
