3afa7428f51ab73a5a04a12553f0ebab6e8016b9
andriy_kovaclhuk <acex1769@icloud.com>
2021-04-27 12:45:19 +0300

    my hw4

--- kovalchuk/HW4/homework_4_6.py
+++ kovalchuk/HW4/homework_4_6.py
@@ -1,11 +1,11 @@
 n = int(input("Number of a place: "))
-if n == 1 or 3 or 5 or 7 or 9 or 11 or 13 or 15 or 17 or 19 or 21 or 23 or 25 or 27 or 29 or 31 or 33 or 35:
+if n in [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35]:
     print("Your place in the bottom of compartment")
-elif n == 2 or 4 or 6 or 8 or 10 or 12 or 14 or 16 or 18 or 20 or 22 or 24 or 26 or 28 or 30 or 32 or 34 or 36:
+elif n in [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36]:
     print("Your place in the top of compartment")
-elif n == 37 or 39 or 41 or 43 or 45 or 47 or 49 or 51 or 53:
+elif n in [37,39,41,43,45,47,49,51,53]:
     print("Your place in the bottom of side")
-elif n == 38 or 40 or 42 or 44 or 46 or 48 or 50 or 52 or 54 or 56:
+elif n in [38,40,42,44,46,48,50,52,54,56]:
     print("Your place in the top of side")
 else:
     print("Incorrect number")
