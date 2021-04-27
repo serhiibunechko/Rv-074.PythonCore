67c99f8cc42baca649dfa1d9ee0cb1c78fda4fd7
Dmytro <dima_mihaliuk@ukr.net>
2021-04-27 15:26:29 +0300

    fix

---
+++ Mykhaliuk/HW3/1.py
@@ -0,0 +1,7 @@
+a = int(input (" a = "))
+b = int(input (" b = "))
+
+a = a+b
+b = a-b
+a = a-b
+print('swapped numbers:','\n a =', a, '\n b =', b)
---
+++ Mykhaliuk/HW3/3.py
@@ -0,0 +1,5 @@
+rectangle_height = float(input (" Enter rectangle height: "))
+rectangle_width = float(input (" Enter rectangle width: "))
+
+perimeter = ( rectangle_height + rectangle_width ) * 2
+print(' perimeter =', perimeter)
